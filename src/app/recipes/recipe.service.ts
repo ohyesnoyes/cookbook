18bf772baefc21d32e4cbaf60286116479cf423a
ohyesnoyes <brothernoyes@gmail.com>
2017-04-17 16:24:30 -0600

    Add routing to nav and dropdown buttons

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,14 +10,14 @@ export class RecipeService {
     private recipes: Recipe[] = [
     new Recipe('Tasty Schnitzel',
                 'A super-tasty Schnitzel - just awesome!',
-                'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+                '../../assets/food1.jpg',
                 [
                     new Ingredient('Meat', 1),
                     new Ingredient('French Fries', 20)
                 ]),
     new Recipe('Big Fat Burger',
                 'What else you need to say?',
-                'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+                '../../assets/food2.jpg',
                 [
                     new Ingredient('Buns', 2),
                     new Ingredient('Meat', 1)
@@ -28,6 +28,10 @@ export class RecipeService {
 
   getRecipes() {
       return this.recipes.slice();
+  }
+
+  getRecipe(index: number){
+      return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
